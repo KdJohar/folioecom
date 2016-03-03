04cc01d8ea14b5a8808ad0810e98053afad5c038
kdjohar <kdjohar91@gmail.com>
2016-03-03 22:34:56 +0530

    page added

--- folio/authentication/views.py
+++ folio/authentication/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response, RequestContext
 from .models import NewsLetter
+from django.http import HttpResponse
+import json
 # Create your views here.
 
 def index(request):
@@ -8,6 +10,12 @@ def index(request):
 
 
 def test(request):
-
-    return render_to_response('index.html')
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        print(email)
+        obj = NewsLetter(email=email)
+        obj.save()
+        return HttpResponse(json.dumps({"nothing to see": "this isn't happening"}),
+            content_type="application/json")
+    return render_to_response('index.html', context_instance=RequestContext(request))
 
