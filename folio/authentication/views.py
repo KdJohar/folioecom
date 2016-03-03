7464b71325804afc0917ce9776e5191392a795d3
kdjohar <kdjohar91@gmail.com>
2016-03-03 18:54:59 +0530

    test file added

--- folio/authentication/views.py
+++ folio/authentication/views.py
@@ -3,9 +3,11 @@ from .models import NewsLetter
 # Create your views here.
 
 def index(request):
-    if request.POST:
-        email = request.POST.get('email')
-        obj = NewsLetter.objects.create(email=email)
-        obj.save()
+
     return render_to_response('comingsoon.html')
+
+
+def test(request):
+
+    return render_to_response('index.html')
 
