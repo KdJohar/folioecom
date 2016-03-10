3f0265118b02c2559a1e887711333d62a5f9d596
kdjohar <kdjohar91@gmail.com>
2016-03-10 21:29:10 +0530

    addded ecom

--- folio/authentication/views.py
+++ folio/authentication/views.py
@@ -1,21 +1,12 @@
 from django.shortcuts import render_to_response, RequestContext
 from .models import NewsLetter
-from django.http import HttpResponse
-import json
+
 # Create your views here.
 
 def index(request):
 
-    return render_to_response('comingsoon.html')
-
-
-def test(request):
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        print(email)
-        obj = NewsLetter(email=email)
-        obj.save()
-        return HttpResponse(json.dumps({"nothing to see": "this isn't happening"}),
-            content_type="application/json")
     return render_to_response('index.html', context_instance=RequestContext(request))
 
+
+def demo(request):
+    return render_to_response('ecommerce/index.html')
